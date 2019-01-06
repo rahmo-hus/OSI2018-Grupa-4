f8e335848ff54975f54514c0ccb79dcd5ce0dd40
SonjaOzegovic <45883408+SonjaOzegovic@users.noreply.github.com>
2019-01-06 20:08:04 +0100

    Update lottery.c
    Added more checks to make sure that the numbers are not repeated

--- lottery.c
+++ lottery.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int check_existing(int array[],int number,int n)
+{
+    int i;
+    for(i=0; i<n; i++)
+        if(array[i]==number)
+            return 1;
+    return 0;
+}
+int check_hits(int array[],int number)
+{
+    int i;
+    for(i=0; i<7; i++)
+        if(array[i]==number)
+            return 1;
+    return 0;
+}
 int lottery()
 {
     printf("Welcome to the LOTTERY!\n");
@@ -18,7 +34,7 @@ int lottery()
     {
         check=0;
         for(i=0; i<7; i++)
-            if(user_numbers[i]<1 || user_numbers[i]>45)
+            if(user_numbers[i]<1 || user_numbers[i]>45)//making sure that user did not enter a number outside number limitations//
             {
                 printf("Your %d. entered number is not correct because it is not between 1 and 45.Please enter a new one:",i+1);
                 scanf("%d",&user_numbers[i]);
@@ -26,7 +42,7 @@ int lottery()
             }
         for(i=0; i<6; i++)
             for(j=i+1; j<7; j++)
-                if(user_numbers[i]==user_numbers[j])
+                if(user_numbers[i]==user_numbers[j])//making sure that user did not enter the same number multiple times//
                 {
                     printf("Your %d. entered number is not correct because you have already entered it as your %d. number.Please enter a new one:",j+1,i+1);
                     scanf("%d",&user_numbers[j]);
@@ -35,10 +51,30 @@ int lottery()
     }
     while(check);
     printf("All the numbers are correct. We will start the LOTTERY now!GOOD LUCK!!!\n");
-    srand(time(NULL));
+    srand(time(NULL));//every time that lottery is played numbers will be different//
+    int k,hits=0;
     for(int i=0; i<20; i++)
     {
         lottery_numbers[i]=rand()%45 +1;
+        do
+        {
+            check=0;
+            while(check_existing(lottery_numbers,lottery_numbers[i],i))//making sure that lottery numbers are all different//
+            {
+                lottery_numbers[i]=rand()%45 +1;
+                check++;
+            }
+            for(j=0;j<i;j++)
+                for(k=0;k<7;k++)
+                if(lottery_numbers[j]==user_numbers[k])
+                hits++;
+            if(hits>4)
+                while(check_hits(user_numbers,lottery_numbers[i]))
+            {
+                lottery_numbers[i]=rand()%45 +1;
+                check++;
+            }
+        }while(check);
         printf("%d. number is: %d\n",i+1,lottery_numbers[i]);
     }
     int number_of_hits=0,points=0,guessed_numbers[7];
@@ -47,9 +83,11 @@ int lottery()
             if(user_numbers[j]==lottery_numbers[i])
             {
                 guessed_numbers[number_of_hits++]=lottery_numbers[i];
-                points+=number_of_hits*10;
+                points+=number_of_hits*10;//points are being given according to what the ordinal number of hit that is//
             }
     printf("Numbers you have guessed are:\n");
+    if(number_of_hits==0)
+        printf("Sorry,you haven't guessed any numbers!\n");
     for(i=0; i<number_of_hits; i++)
         printf("%d ",guessed_numbers[i]);
     printf("\nYour score for this round of LOTTERY is: %d\n",points);
